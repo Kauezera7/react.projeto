8cf8cd446c7872e34cf609d197d596eca8665669
Kauezera7 <krc.omega@gmail.com>
2026-02-17 16:07:35 -0300

    seta

--- shineray-react/src/components/ProductCarousel.jsx
+++ shineray-react/src/components/ProductCarousel.jsx
@@ -8,45 +8,56 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+// Ícones SVG solicitados
+const CaretLeft = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+    <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+  </svg>
+);
+
+const CaretRight = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+    <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+  </svg>
+);
+
 const ProductCarousel = () => {
   return (
-    <div className="product-carousel">
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-          1280: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-          },
-          1600: {
-            slidesPerView: 5,
-            spaceBetween: 30,
-          },
-        }}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        className="productSwiper"
-      >
-        {productsData.map((product, index) => (
-          <SwiperSlide key={product.id}>
-            <ProductCard product={product} index={index} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="product-carousel-container-outer">
+      <button className="custom-swiper-btn prev-btn" id="featured-prev">
+        <CaretLeft />
+      </button>
+
+      <div className="product-carousel-inner">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={20}
+          slidesPerView={1}
+          navigation={{
+            nextEl: '#featured-next',
+            prevEl: '#featured-prev',
+          }}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            640: { slidesPerView: 2, spaceBetween: 20 },
+            1024: { slidesPerView: 3, spaceBetween: 30 },
+            1280: { slidesPerView: 4, spaceBetween: 30 },
+            1600: { slidesPerView: 5, spaceBetween: 30 },
+          }}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          className="productSwiper"
+        >
+          {productsData.map((product, index) => (
+            <SwiperSlide key={product.id}>
+              <ProductCard product={product} index={index} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
+      <button className="custom-swiper-btn next-btn" id="featured-next">
+        <CaretRight />
+      </button>
     </div>
   );
 };
--- shineray-react/src/pages/ProductDetail.jsx
+++ shineray-react/src/pages/ProductDetail.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper/modules';
+import { Navigation, Autoplay, Pagination } from 'swiper/modules';
 import { 
   MessageCircle, ArrowLeft, ShieldCheck, 
   Zap, Bike, Fuel, Gauge, CreditCard,
@@ -15,6 +15,20 @@ import ProductCard from '../components/ProductCard';
 // Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// Ícones SVG solicitados
+const CaretLeft = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+    <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+  </svg>
+);
+
+const CaretRight = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+    <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+  </svg>
+);
 
 const ProductDetail = () => {
   const { slug } = useParams();
@@ -163,27 +177,41 @@ const ProductDetail = () => {
             <h2>Motos que você <span className="highlight">pode gostar</span></h2>
           </div>
           
-          <div className="related-carousel-wrapper">
-            <Swiper
-              modules={[Navigation, Autoplay]}
-              spaceBetween={30}
-              slidesPerView={1}
-              navigation
-              autoplay={{ delay: 4000 }}
-              breakpoints={{
-                640: { slidesPerView: 2 },
-                1024: { slidesPerView: 3 },
-                1280: { slidesPerView: 4 },
-                1600: { slidesPerView: 5 }
-              }}
-              className="relatedSwiper"
-            >
-              {relatedProducts.map((relProduct, index) => (
-                <SwiperSlide key={relProduct.id}>
-                  <ProductCard product={relProduct} index={index} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
+          <div className="product-carousel-container-outer">
+            <button className="custom-swiper-btn prev-btn" id="related-prev">
+              <CaretLeft />
+            </button>
+
+            <div className="product-carousel-inner">
+              <Swiper
+                modules={[Navigation, Autoplay, Pagination]}
+                spaceBetween={30}
+                slidesPerView={1}
+                navigation={{
+                  nextEl: '#related-next',
+                  prevEl: '#related-prev',
+                }}
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 4000 }}
+                breakpoints={{
+                  640: { slidesPerView: 2 },
+                  1024: { slidesPerView: 3 },
+                  1280: { slidesPerView: 4 },
+                  1600: { slidesPerView: 5 }
+                }}
+                className="relatedSwiper"
+              >
+                {relatedProducts.map((relProduct, index) => (
+                  <SwiperSlide key={relProduct.id}>
+                    <ProductCard product={relProduct} index={index} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+
+            <button className="custom-swiper-btn next-btn" id="related-next">
+              <CaretRight />
+            </button>
           </div>
         </section>
       </div>
